60c545d0896444cdfc34bd667d5f35795841d4e1
RyoKitatani <ryo0929m@gmail.com>
2021-05-16 15:40:02 +0900

    Initial Commit

---
+++ LocalizationsApp/LocalizationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationsAppApp.swift
+//  LocalizationsApp
+//
+//  Created by RyoKitatani on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
